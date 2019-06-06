31049b37855a67d9b7c1e8484b393994f7c4bfd0
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2019-06-06 21:33:28 +0200

    Extract logic to ArrayMatrix

---
+++ src/TRegx/CrossData/ArrayMatrix.php
@@ -0,0 +1,48 @@
+<?php
+namespace TRegx\CrossData;
+
+class ArrayMatrix
+{
+    public function cross(array $dataProviders): array
+    {
+        if (count($dataProviders) === 1) {
+            return $dataProviders[0];
+        }
+        $result = array_shift($dataProviders);
+        $result = $this->quoteKeys($result);
+        foreach ($dataProviders as $dataProvider) {
+            $result = $this->crossTwo($result, $dataProvider);
+        }
+        return $result;
+    }
+
+    public function crossTwo(array $array1, array $array2): array
+    {
+        $result = [];
+        foreach ($array1 as $key1 => $value1) {
+            foreach ($array2 as $key2 => $value2) {
+                $result[self::keyName($key1, $key2)] = array_merge($value1, $value2);
+            }
+        }
+        return $result;
+    }
+
+    private function keyName($previous, $new): string
+    {
+        $keys = json_decode($previous);
+        if (!is_array($keys)) {
+            $keys = [$previous];
+        }
+        $keys[] = $new;
+        return json_encode($keys);
+    }
+
+    private function quoteKeys(array $array): array
+    {
+        $result = [];
+        foreach ($array as $key => $value) {
+            $result[json_encode([$key])] = $value;
+        }
+        return $result;
+    }
+}
