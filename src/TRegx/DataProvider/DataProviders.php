f85b523e51904b3ac0227a936c46f34a5c0328ce
Oliver Klee <github@oliverklee.de>
2021-05-27 22:10:46 +0200

    Keep all lines shorter than 120 characters
    This brings the code one step forward towards PSR-12 compliance.

--- src/TRegx/DataProvider/DataProviders.php
+++ src/TRegx/DataProvider/DataProviders.php
@@ -56,7 +56,9 @@ class DataProviders
     {
         foreach ($entries as $value) {
             if (!is_array($value)) {
-                throw new \InvalidArgumentException(sprintf("Argument list is supposed to be an array, '%s' given", gettype($value)));
+                throw new \InvalidArgumentException(
+                    sprintf("Argument list is supposed to be an array, '%s' given", gettype($value))
+                );
             }
             if (array_values($value) !== $value) {
                 throw new \InvalidArgumentException("Arguments composed of an associative array");
